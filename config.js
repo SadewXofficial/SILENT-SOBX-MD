268cc48c60cfb37a911e4c7ab0189fd76343bde1
SadewXofficial <160341086+SadewXofficial@users.noreply.github.com>
2024-11-08 15:27:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your session",
-MONGODB: process.env.MONGODB || "enter mongodb url",
+SESSION_ID: process.env.SESSION_ID || "vnJlQaAB#W84lEe3iug5q-Uqv6qR73b4m4WBo4EK3Rb1ENjqjMD0",
+MONGODB: process.env.MONGODB || "mongodb+srv://chuutiya309:ldQNSiK8uSivRslU@cluster0.jx8wm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
 };
